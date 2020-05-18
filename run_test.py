126202b1ab39b2f82e4e14c5161826a028be4cc7
Felipe Matheus Pinto <felipe_matheus.p@hotmail.com>
2020-05-17 21:19:20 -0300

    Random Forest Running

--- run_test.py
+++ run_test.py
@@ -1,17 +1,32 @@
-from segment import readimg,segmentation
+from scripts import readimg,segmentation,classification
 import os
+import numpy as np
+import time
 
 if __name__=='__main__':
-    try:
-        im=r'samples/Ortomosaico.tif'
-        img=readimg.imread(im)
-        s=segmentation.Seg(img)
-        s.SLIC(100,1.0,1.0,os.path.join(r'samples/output','SLIC.tif'))
-    except MemoryError as e:
-        print(e)
+    # try:
+    #     im=r'samples/Ortomosaico.tif'
+    #     img=readimg.imread(im)
+    #     s=segmentation.Seg(img)
+    #     s.SLIC(100,1.0,1.0,os.path.join(r'samples/output','SLIC.tif'))
+    # except MemoryError as e:
+    #     print(e)
 
-    print("END!")
-    while True:
-        pass
+    # print("END!")
+    # while True:
+    #     pass
+    im=r'samples/Seg_FalsaCorTGI.tif'
+    img=readimg.imread(im)
+    shp=r'samples/AmostrasTGI2.shp'
+    imOut=r'samples/output/Classified.tif'
+
+    t1=time.time()
+    c=classification.classify(img)
+    X, X_test, y, y_test = c.get_samples(shp,column='Classvalue',trainTest=0.25)
+    r=c.randomForest(X,y,imOut,X_test=X_test,y_test=y_test)
+    t2=time.time()
+    print(f"Classification finished in {t2-t1} seconds with accuracy of {r}")
+
+
 
 
--- scripts/classification.py
+++ scripts/classification.py
@@ -11,6 +11,8 @@ from sklearn.cluster import KMeans,MiniBatchKMeans
 from shapely.geometry import mapping
 import geopandas as gpd
 from rasterio.mask import mask
+import rasterio as rio
+from sklearn.metrics import accuracy_score
 
 
 class classify:
@@ -20,41 +22,38 @@ class classify:
         if bands==None:
             bands=[i+1 for i in range(img.img.RasterCount)]
         self.bands=bands
+        s=np.shape(rio.open(img.img_name).read())
+        self.img.shape=[s[1],s[2],len(self.bands)]
+        #print(self.img.shape)
 
     def kmeans(self):
         pass
 
-    def get_samples(self)
-
     
-    def randomForest(self,X,y,imOut,n_arvores=100,profundidade=2):
+    def randomForest(self,X,y,imOut,n_arvores=100,profundidade=2,X_test=None,y_test=None):
 
         clf = RandomForestClassifier(n_estimators=n_arvores, max_depth=profundidade,random_state=0)
         clf.fit(X, y) 
 
-        srcIm=np.reshape(self.img.array,(self.img.shape[0]*self.img.shape[1],self.img.nBandas))
+        srcIm=np.reshape(self.img.array,(self.img.shape[0]*self.img.shape[1],self.img.shape[2]))
 
         classificado=clf.predict(srcIm)
         outIm=np.reshape(classificado,(self.img.shape[0],self.img.shape[1]))
         
-        self.img.newIm(outIm,imOut)
+        self.img.new_img(outIm,imOut)
 
-        if acuracia==True:
-            if X_test==None:
-                raise Exception("Insira valores para X_test")
-            if y_test==None:
-                raise Exception("Insira valores para y_test")
+        if X_test is not None and y_test is not None:
             pred=clf.predict(X_test)
-            print("Acurácia da classificação: ",accuracy_score(y_test, pred))
+            return accuracy_score(y_test, pred)
 
 
-    def coletaDeAmostras(self,vector_file,trainTest=0.0,noData=256):
+    def get_samples(self,vector_file,column='id',trainTest=0.0,noData=256):
         img=self.img.img_name
         shapefile = gpd.read_file(vector_file)
         X=[]
         y=[]
-        cont=1
-        total=len(shapefile)
+        #cont=1
+        #total=len(shapefile)
         for i in range(len(shapefile['geometry'])):
             with rio.open(img) as imgOriginal:
                 try:
@@ -73,15 +72,15 @@ class classify:
                     merge=cv2.merge(imgRec)
                     re=np.reshape(merge,(shape[1]*shape[2],shape[0]))
                     no=np.array([noData]*shape[0],dtype=re.dtype)
-                    re=re[re!=arr]
-                    XTemp=np.reshape(rr,(int(np.shape(rr)[0]/shape[0]),shape[0]))
+                    re=re[re!=no]
+                    XTemp=np.reshape(re,(int(np.shape(re)[0]/shape[0]),shape[0]))
 
             for l in XTemp:
-                if l not in X:                 
-                    X.append(l)
-                    y.append(shapefile['id'][i])
-            print(f"Loop {cont} of {total} done!")
-            cont+=1
+                # if l not in X:
+                X.append(l)
+                y.append(shapefile[column][i])
+            #print(f"Loop {cont} of {total} done!")
+            #cont+=1
         if trainTest==0:
             return X,y
         elif trainTest>=1:
